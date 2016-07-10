28c0bf39b661fef9a0491aa91fbb6fc18c6e4348
Sairam Nutheti <sai.nutheti@gmail.com>
2016-07-10 02:14:27 -0700

    index page updated

--- Paypal/routes/index.js
+++ Paypal/routes/index.js
@@ -1,8 +1,41 @@
+var http = require('http');
+var URL = require('url');
+var jsforce = require('jsforce');
+var config = require('../config/config.json');
 
 /*
  * GET home page.
  */
-
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  var query = "select name from location__c"
+  console.log(query);
+  var conn = new jsforce.Connection();
+  var connection=conn.login(config.username, config.password, function(err, connRes) {
+    if (err) { return console.error(err); }
+    else{
+      console.log("Connection Successful");
+      conn.query(query, function(err, countries) {
+        if (err) {
+          console.error(err);
+          res.send(err);
+        } else {
+          var connection=conn.login(config.username, config.password, function(err, connRes) {
+            if (err) { return console.error(err); }
+            else {
+              conn.query(query, function (err, mTypes) {
+                if (err) {
+                  console.error(err);
+                  res.send(err);
+                } else {
+                  console.log(res1);
+                  res.render('index', {'countries':countries, 'memberTypes':mTypes});
+                }
+              });
+            }
+          });
+        }
+      });
+    }
+  });
+
 };
